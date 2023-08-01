c052ea0630c34a7af397ad6d89b80b5c177f403c
Alvin J. Alexander <alvin@alexander.jp>
2023-08-01 22:03:43 +0200

    3 nodes created and linked

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: musenov <musenov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 21:32:42 by musenov           #+#    #+#             */
-/*   Updated: 2023/07/31 20:59:21 by musenov          ###   ########.fr       */
+/*   Updated: 2023/08/01 21:59:09 by musenov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,51 +24,57 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+typedef enum e_ast_node_type
+{
+	AST_NODE_CMD,
+	AST_NODE_PIPE
+}	t_ast_node_type;
+
+typedef enum e_redirect_type
+{
+	REDIRECT_STDIN,
+	REDIRECT_STDOUT,
+	REDIRECT_HERE_DOC,
+	REDIRECT_STDOUT_APPEND
+}	t_redirect_type;
+
+typedef struct s_redirect
+{
+	t_redirect_type		type;
+	char				*word;
+	struct s_redirect	*next;
+}	t_redirect;
+
+typedef struct s_assignment
+{
+	char				*word;
+	struct s_assignment	*next;
+}	t_assignment;
+
+typedef struct s_ast_node_content
+{
+	t_redirect		*stdin_redirect;
+	t_redirect		*stdout_redirect;
+	t_assignment	*assignments;
+	char			**cmd;
+}	t_ast_node_content;
+
 typedef struct s_ast_node
 {
 	t_ast_node_type		type;
 	t_ast_node_content	*content;
-	t_ast_node			*left;
-	t_ast_node			*right;
+	struct s_ast_node	*left;
+	struct s_ast_node	*right;
 }	t_ast_node;
 
-		typedef enum e_ast_node_type
-		{
-			AST_NODE_CMD,
-			AST_NODE_PIPE
-		}	t_ast_node_type;
+// main.c
 
-		typedef struct s_ast_node_content
-		{
-			t_redirect		*stdin_redirect;
-			t_redirect		*stdout_redirect;
-			t_assignment	*assignments;
-			char			**cmd;
-		}	t_ast_node_content;
-
-				typedef struct s_redirect
-				{
-					t_redirect_type	type;
-					char			*word;
-					t_redirect		*next;
-				}	t_redirect;
-
-						typedef enum e_redirect_type
-						{
-							REDIRECT_STDIN,
-							REDIRECT_STDOUT,
-							REDIRECT_HERE_DOC,
-							REDIRECT_STDOUT_APPEND
-						}	t_redirect_type;
-
-				typedef struct s_assignment
-				{
-					char			*word;
-					t_assignment	*next;
-				}	t_assignment;
-
-
-int		main(void);
-
+int					main(void);
+t_ast_node			*create_ast(void);
+t_ast_node			*create_node(void);
+void				init_node(t_ast_node *new_node, char *node_type, char **cmd, \
+							t_ast_node *new_node_left, t_ast_node *new_node_right);
+t_ast_node_content	*create_node_content(void);
+void				init_node_content(char **cmd, t_ast_node *new_node);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: musenov <musenov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 20:46:03 by musenov           #+#    #+#             */
-/*   Updated: 2023/08/01 13:58:04 by musenov          ###   ########.fr       */
+/*   Updated: 2023/08/01 22:03:05 by musenov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,9 @@ int	main(void)
 		free(line); // Free the memory allocated by readline
 		line = readline("minishell>");
 	}
-	create_ast();
+	head = create_ast();
 	return (0);
 }
-
-
-
-
 
 /*
 
@@ -44,11 +40,23 @@ ls -la | grep 1 | wc -c
 
 */
 
-void	create_ast(t_ast_node *head)
+t_ast_node	*create_ast(void)
 {
-	
-}
+	char		**cmd;
+	t_ast_node	*new_node_left;
+	t_ast_node	*new_node_right;
+	t_ast_node	*new_node_head;
 
+	cmd = ft_split("ls -la", ' ');
+	new_node_left = create_node();
+	init_node(new_node_left, "AST_NODE_CMD", cmd, NULL, NULL);
+	cmd = ft_split("grep 1", ' ');
+	new_node_right = create_node();
+	init_node(new_node_right, "AST_NODE_CMD", cmd, NULL, NULL);
+	new_node_head = create_node();
+	init_node(new_node_head, "AST_NODE_PIPE", NULL, new_node_left, new_node_right);
+	return (new_node_head);
+}
 
 t_ast_node	*create_node(void)
 {
@@ -57,70 +65,44 @@ t_ast_node	*create_node(void)
 	new_node = malloc(sizeof(*new_node));
 	if (new_node == NULL)
 		exit(EXIT_FAILURE);
+	// new_node->type = NULL;
 	new_node->content = NULL;
 	new_node->left = NULL;
 	new_node->right = NULL;
 	return (new_node);
 }
 
-
-void	add_node(t_ast_node *head)
+void	init_node(t_ast_node *new_node, char *node_type, char **cmd, \
+				t_ast_node *new_node_left, t_ast_node *new_node_right)
 {
-	t_ast_node	*new_node;
-	t_ast_node	*current;
-
-	new_node = create_node();
-	if (*head == NULL)
+	new_node->type = *node_type;
+	if (cmd)
 	{
-		*head = new_node;
-		return ;
+		new_node->content = create_node_content();
+		init_node_content(cmd, new_node);
 	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new_node;
-	new_node->prev = current;
+	else
+	{
+		new_node->left = new_node_left;
+		new_node->right = new_node_right;
+	}
 }
 
-void	add_node(t_node **head, int num)
+t_ast_node_content	*create_node_content(void)
 {
-	t_node	*new_node;
-	t_node	*current;
+	t_ast_node_content	*new_node;
 
-	new_node = create_node(num);
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return ;
-	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new_node;
-	new_node->prev = current;
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		exit(EXIT_FAILURE);
+	new_node->stdin_redirect = NULL;
+	new_node->stdout_redirect = NULL;
+	new_node->assignments = NULL;
+	new_node->cmd = NULL;
+	return (new_node);
 }
 
-int	count_nodes(t_node *stack)
+void	init_node_content(char **cmd, t_ast_node *new_node)
 {
-	t_node	*node;
-	int		count;
-
-	node = stack;
-	count = 0;
-	while (node)
-	{
-		count++;
-		node = node->next;
-	}
-	return (count);
-}
-
-t_node	*find_last_node(t_node *head)
-{
-	t_node	*current;
-
-	current = head;
-	while (current != NULL && current->next != NULL)
-		current = current->next;
-	return (current);
+	new_node->content->cmd = cmd;
 }
