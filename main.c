d4c8f2fd89e143d1706b47c8a6656ab257adfb6a
nnavidd <navid.nabaeei@gmail.com>
2023-08-29 20:16:03 +0200

    fixing the leaks

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nnavidd <nnavidd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 20:46:03 by musenov           #+#    #+#             */
-/*   Updated: 2023/08/29 13:28:16 by nnavidd          ###   ########.fr       */
+/*   Updated: 2023/08/29 20:15:09 by nnavidd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,19 @@
 int	main(void)
 {
 	char		*line;
-	// int			token_count;
-	// t_ast_node	*ast_root;
-	// t_token		*tokens;
 	t_minishell	shell_data;
 
-	// sh = NULL;
 	init_shell(&shell_data);
-	// ast_root = shell_data.ast_root;
 	line = readline("minishell>");
 	while (line)
 	{
 		if (line[0] != '\0')
 		{
 			add_history(line);
-			// tokenize(&shell_data.tokens, line, &shell_data.token_len);
 			tokenize(&shell_data, line);
-			// print_tokens(tokens, token_count);
 			print_tokens(shell_data);
-	
-			// tokens = shell_data.tokens;
-			// token_count = shell_data.token_len;
-			// ast_root = parse_pipeline(&tokens, &token_count);
 			shell_data.ast_root = parse_pipeline(&shell_data);
-
 			free_tokens(&shell_data);
-			
 			print_ast_node(shell_data.ast_root, 1, 'x');
 			free_ast(&shell_data.ast_root);
 			printf("You entered: %s\n", line); // use the line
