20edc4acfb4555684fea0ba1a130af7e0f524fc7
Alvin J. Alexander <alvin@alexander.jp>
2023-10-13 21:08:20 +0200

    (1) 407 failed, 2398 passed; (2) working on pipeline 13 failed test cases.

---
+++ test_cases1/1_builtins_failed.sh
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                   BUILTINS                                   #
+# **************************************************************************** #
+
+cd "doesntexist" 2>/dev/null 
+
+cd "wtf" 2>/dev/null 
+
+export
+
+export --TEST=123
+
+export -TEST=100
+
+export -TEST=123
+
+export TEST+=100
+
+unset TES;T
+
+unset -TEST
