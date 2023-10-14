b99767c33e3951ba40fa4e43fcfda3a35e203d23
Alvin J. Alexander <alvin@alexander.jp>
2023-10-14 15:04:14 +0200

    (1) 217 failed, 2588 passed; (2) main() cleaned from commented blocks of code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: musenov <musenov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 16:30:44 by musenov           #+#    #+#             */
-/*   Updated: 2023/10/14 14:23:32 by musenov          ###   ########.fr       */
+/*   Updated: 2023/10/14 14:59:00 by musenov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,64 +39,35 @@ int	main(int argc, char **argv)
 			line = ft_strtrim(line_bla, "\n");
 			free(line_bla);
 		}
-		// exit_code_signals(&data);
 		set_signals_noninteractive(&data);
-		// exit_code_signals(&data);
 		if (line == NULL || *line == EOF)
 		{
 			free_envp_ll(shell_data.envp_ll);
 			free_envp_local(shell_data.envp_local);
 			free_tokens(&shell_data);
-			// exit_code_signals(&data);
 			return (data.exit_code);
 		}
 		if (line[0] != '\0')
 		{
 			add_history(line);
-			// if (tokenize(&shell_data, line) == LEXER_SUCCESS)
-			// {
-				// shell_data.ast_root = parse_pipeline(&shell_data);
-				shell_data.ast_root = parsing(&shell_data, line);
-				// print_ast_tree0(shell_data.ast_root, 0);
-				// print_envp_ll(shell_data.envp_ll);
-				// print_envp_local(shell_data.envp_local);
-				// if (shell_data.ast_root)
-				// {
-				// 	free_tokens(&shell_data);
-					i = 0;
-					if (shell_data.ast_root && shell_data.ast_root->type == AST_NODE_CMD)
-					{
-						piper(&data, &i);
-						forker_no_pipe(&data, shell_data.envp_local, shell_data.ast_root);
-						// free_ast(&shell_data.ast_root);
-						free_ast(shell_data.ast_root);
-					}
-					else
-					{
-						data.nr_of_cmd_nodes = 0;
-						// execute_cmds(shell_data.ast_root, &i, &data, shell_data.envp_local);
-						if(!execute_cmds(shell_data.ast_root, &i, &data, shell_data.envp_local))
-						{
-							// free_ast(&shell_data.ast_root);
-							free_ast(shell_data.ast_root);
-							continue ;
-						}
-						// free_ast(&shell_data.ast_root);
-						free_ast(shell_data.ast_root);
-					}
-			// 	}
-			// 	else
-			// 	{
-			// 		printf("PARSER FAILED\n");
-			// 		free_tokens(&shell_data);
-			// 		free_ast(&shell_data.ast_root);
-			// 	}
-			// }
-			// else
-			// {
-			// 	printf("LEXER FAILED\n");
-			// 	free_tokens(&shell_data);
-			// }
+			shell_data.ast_root = parsing(&shell_data, line);
+			i = 0;
+			if (shell_data.ast_root && shell_data.ast_root->type == AST_NODE_CMD)
+			{
+				piper(&data, &i);
+				forker_no_pipe(&data, shell_data.envp_local, shell_data.ast_root);
+				free_ast(shell_data.ast_root);
+			}
+			else
+			{
+				data.nr_of_cmd_nodes = 0;
+				if(!execute_cmds(shell_data.ast_root, &i, &data, shell_data.envp_local))
+				{
+					free_ast(shell_data.ast_root);
+					continue ;
+				}
+				free_ast(shell_data.ast_root);
+			}
 		}
 		ft_waiting(&data);
 		free(line);
