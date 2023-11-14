4c9f0afcc8efd3d9ae483cc19d41eba9420a56f5
Alvin J. Alexander <alvin@alexander.jp>
2023-11-14 20:50:24 +0100

    (1) 63 fails, no aborts; (2) working on leaks - added free(temp) after i=0 if (access(temp, X_OK) == -1) in  prepare_cmd_path().

--- 04_executor/child_process_utils0.c
+++ 04_executor/child_process_utils0.c
@@ -6,7 +6,7 @@
 /*   By: musenov <musenov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 13:21:08 by musenov           #+#    #+#             */
-/*   Updated: 2023/11/13 19:30:21 by musenov          ###   ########.fr       */
+/*   Updated: 2023/11/14 20:44:22 by musenov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,9 +67,9 @@ void	prepare_cmd_path(t_pipe *data)
 		return ;
 	}
 	i = 0;
+	free(temp);
 	while (data->paths[i])
 	{
-		free(temp);
 		temp = ft_strjoin("/", data->cmd_split[0]);
 		cmd_path_func = ft_strjoin(data->paths[i], temp);
 		if (access(cmd_path_func, X_OK) != -1)
@@ -78,6 +78,7 @@ void	prepare_cmd_path(t_pipe *data)
 			break ;
 		}
 		free(cmd_path_func);
+		free(temp);
 		i++;
 	}
 	if (data->paths[i] == NULL)
